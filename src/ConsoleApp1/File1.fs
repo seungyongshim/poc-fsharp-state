f586040999a85979f7f86bb7d4c8698343cc7c50
SeungYongShim <syshim@me.com>
2022-12-18 22:24:28 +0900

    .

--- src/ConsoleApp1/File1.fs
+++ src/ConsoleApp1/File1.fs
@@ -5,7 +5,8 @@ open System
 type State<'S, 'A> = (struct ('S * 'A))
 type Random = State<double, double>
 
-module State = 
+module State =
+    let map
     let random (seed : double) : Random = 
         let nextSeed = (1839567234. * seed + 972348567.) % 8239451023.
         (nextSeed, nextSeed)
